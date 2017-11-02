9e534aaa01f69b35c9258215e3b32e3ec7a53e99
d3x0r <d3x0r@users.noreply.github.com>
2017-11-02 13:10:19 -0700

    Extend test a little

--- test.js
+++ test.js
@@ -2,11 +2,15 @@ var Gun = require( 'gun' );
 require( '.' );
 
 var gun = new Gun();
-var member = gun.get( "member" ).put( { name: "fred" } );
+var member = gun.get( "member" ).put( { name: "alex" } );
+var member2 = gun.get( "member2" ).put( { name: "betty" } );
+var member3 = gun.get( "member3" ).put( { name: "cindy" } );
 var root = gun.get( 'something' );
 
 root.on( (v,f)=>{console.log( f,'=',v )} );
 
 root.set( member );
+root.set( member2 );
+root.set( member3 );
 root.unset( member );
 
